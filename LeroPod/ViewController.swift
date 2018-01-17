a794deeb0000f9474ce91d95741c338c61dea272
Swift <swift@wozmac3.local>
2018-01-17 20:08:01 -0200

    Initial Commit

---
+++ LeroPod/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeroPod
+//
+//  Created by Swift on 17/01/2018.
+//  Copyright © 2018 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
